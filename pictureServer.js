7cab1af10079a9b16a9bc6a0a15d55be0e2c0a75
emtseng <etseng42@gmail.com>
2018-02-08 13:36:30 -0500

    modifies server.js

--- pictureServer.js
+++ pictureServer.js
@@ -111,19 +111,19 @@ io.on('connect', function(socket) {
   });
 
   //-- Addition: This function is called when the client clicks on the `Take a picture` button.
-  socket.on('takePicture', function() {
+  socket.on('light', function () {
     /// First, we create a name for the new picture.
     /// The .replace() function removes all special characters from the date.
     /// This way we can use it as the filename.
     var imageName = new Date().toString().replace(/[&\/\\#,+()$~%.'":*?<>{}\s-]/g, '');
 
-    console.log('making a making a picture at'+ imageName); // Second, the name is logged to the console.
+    console.log(`making a picture at ${imageName}`); // Second, the name is logged to the console.
 
     //Third, the picture is  taken and saved to the `public/`` folder
-    NodeWebcam.capture('public/'+imageName, opts, function( err, data ) {
-    io.emit('newPicture',(imageName+'.jpg')); ///Lastly, the new name is send to the client web browser.
-    /// The browser will take this new name and load the picture from the public folder.
-  });
+    NodeWebcam.capture(`public/${imageName}`, opts, (err, data) => {
+      io.emit('newPicture', (`${imageName}.jpg`)); ///Lastly, the new name is send to the client web browser.
+      /// The browser will take this new name and load the picture from the public folder.
+    });
 
   });
   // if you get the 'disconnect' message, say the user disconnected
